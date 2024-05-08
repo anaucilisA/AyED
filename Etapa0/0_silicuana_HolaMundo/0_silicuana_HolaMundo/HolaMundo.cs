3f8ba228ea460d17c393813f7bab72df39b09d75
anaucilisA <167359079+anaucilisA@users.noreply.github.com>
2024-05-08 14:56:03 -0300

    Add files via upload

---
+++ Etapa0/0_silicuana_HolaMundo/0_silicuana_HolaMundo/HolaMundo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_silicuana_HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola mundo");
+        }
+    }
+}
---
+++ Etapa0/1_silicuana_HolaNombre/1_silicuana_HolaNombre/HolaNombre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_silicuana_HolaNombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine("ingrese su nombre porfavor");
+            nombre = Console.ReadLine();
+            Console.WriteLine("hola " + nombre);
+
+            Console.ReadKey();
+
+        }
+    }
+}
