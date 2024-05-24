b77c37089ceac38ee2190ea0bf6e0e7b467c9be0
anaucilisA <167359079+anaucilisA@users.noreply.github.com>
2024-05-24 16:25:15 -0300

    Add files via upload

---
+++ Etapa1/15_silicuana_CuentaRegresiva/15_silicuana_CuentaRegresiva/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_silicuana_CuentaRegresiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("dame un numero");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine("_______________________");
+            for (int i=0 ;i<num+1;i++)
+            {
+                Console.WriteLine(num - i);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Etapa1/16_silicuana_FizzBuzz/16_silicuana_FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_silicuana_FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            while (num < 101)
+            {
+                if (!(num % 3 == 0 & num % 5 == 0))
+                {
+                    if (!(num % 5 == 0))
+                    {
+                        if (num % 3 == 0)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("fizz");
+
+
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine(num);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("buzz");
+                    }
+
+
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("fizzbuzz");
+                }
+
+                num++;
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
