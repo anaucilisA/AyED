2fec9826493b34a09137cab88d5852660d681fc4
anaucilisA <167359079+anaucilisA@users.noreply.github.com>
2024-05-24 16:14:06 -0300

    Add files via upload

---
+++ Etapa1/13_silicuana_DiezVeces/13_silicuana_DiezVeces/DiezVeces.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_silicuana_DiezVeces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String palabra;
+            Console.WriteLine("ingrese na palabra");
+            palabra = Console.ReadLine();
+            Console.WriteLine("-----------------------------");
+
+            int contador;
+            contador = 0;
+
+            while ( contador < 10)
+            {
+                Console.WriteLine(palabra);
+                contador++;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
