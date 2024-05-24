a39e8af29b27d6130b0cdd2fc9911b98dcf2c4ea
anaucilisA <167359079+anaucilisA@users.noreply.github.com>
2024-05-24 14:41:11 -0300

    Add files via upload

---
+++ Etapa1/7_silicuana_AlReves/7_silicuana_AlReves/AlReves.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_silicuana_AlReves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letra1;
+            string letra2;
+            string letra3;
+
+            Console.WriteLine("ingrese sus letras");
+            letra1 = Console.ReadLine();
+            letra2 = Console.ReadLine();
+            letra3 = Console.ReadLine();
+
+            Console.WriteLine("---------");
+
+            Console.WriteLine(letra3);
+            Console.WriteLine(letra2);
+            Console.WriteLine(letra1);
+
+            Console.ReadKey();
+        }
+    }
+}
