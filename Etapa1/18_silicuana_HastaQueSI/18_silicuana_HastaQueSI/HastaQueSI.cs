16e53aba4f63023211811a965c0c938047d024ca
anaucilisA <167359079+anaucilisA@users.noreply.github.com>
2024-05-24 16:34:24 -0300

    Add files via upload

---
+++ Etapa1/18_silicuana_HastaQueSI/18_silicuana_HastaQueSI/HastaQueSI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_silicuana_HastaQueSI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool buc = true;
+            string respuesta;
+            string si = "SI";
+            while (buc)
+            {
+                Console.WriteLine("diga SI para terminar");
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("Desea terminar el programa");
+                respuesta = Console.ReadLine();
+
+                if (respuesta == si)
+                {
+                    Console.WriteLine("precione cualquier tecla");
+                    buc = false;
+                }
+
+            }
+            //fin
+            Console.ReadKey();
+        }
+    }
+}
