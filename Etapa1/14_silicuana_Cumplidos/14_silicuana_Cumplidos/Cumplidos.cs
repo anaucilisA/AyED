29b47db1ed006abc44160c07c962463cedb0f73f
anaucilisA <167359079+anaucilisA@users.noreply.github.com>
2024-05-24 16:23:21 -0300

    Add files via upload

---
+++ Etapa1/14_silicuana_Cumplidos/14_silicuana_Cumplidos/Cumplidos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_silicuana_Cumplidos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cuantos años tenes");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            int edad2 = edad + edad;
+            int num = 1;
+            while (edad < edad2)
+            {
+                Console.WriteLine((edad2 - edad - (edad - num)));
+                edad2--;
+                num = num + 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
