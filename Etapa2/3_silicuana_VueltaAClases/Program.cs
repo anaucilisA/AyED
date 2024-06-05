07aee3ab620a71b6f1b949ceab27361697d374cf
anaucilisA <167359079+anaucilisA@users.noreply.github.com>
2024-06-05 15:07:36 -0300

    Add files via upload

---
+++ Etapa2/3_silicuana_VueltaAClases/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_silicuana_VueltaAClases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int notafinal = 0;
+            int aprobados = 0;
+            int porcentaje = 0;
+            int pruebas;
+            int tp;
+            int nota;
+
+            Console.Write("ingrese la cantidad de pruebas ");
+            pruebas = int.Parse(Console.ReadLine());
+            Console.Write("ingrese la cantidad de tp ");
+            tp = int.Parse(Console.ReadLine());
+            int[] laspruebas = new int[pruebas];
+            int[] tps = new int[tp];
+            for (int i = 0; i < pruebas; i++)
+            {
+                Console.Write("ingrese las nota de la prueba" + (i + 1 ) + " = ");
+               nota = int.Parse(Console.ReadLine());
+                laspruebas[i] = nota;
+            }
+            for (int i = 0; i < pruebas; i++)
+            {
+                notafinal = notafinal + laspruebas[i];
+            }
+            for (int i = 0; i < tp; i++)
+            {
+                Console.Write("ingrese la nota del tp " + (i + 1 ) + " = ");
+                nota = int.Parse(Console.ReadLine());
+                tps[i] = nota;
+                if (tps[i] >= 6)
+                {
+                    porcentaje++;
+                }
+            }
+            aprobados = (int)(0.75*tp);
+            if ((notafinal) >= 6 && porcentaje >= (aprobados))
+            {
+                Console.Write("aprobaron");
+            }
+            else
+            {
+                Console.Write("no puede aprobar");
+            }
+            Console.ReadKey();
+            /*
+           El promedio de las notas de los exámenes debe ser mayor o igual a 6.
+             Al menos el 75% de los TPs deben tener una nota igual o mayor a 6.
+             */
+        }
+    }
+}
---
+++ Etapa2/4_silicuana_PixelDreams/4_silicuana_PixelDreams/PixelDreams.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_silicuana_PixelDreams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int jugadores;
+            Console.Write("ingrese la cantidad de jugadores =  ");
+            jugadores = int.Parse(Console.ReadLine());
+            int[] losJugadores = new int[jugadores];
+            bool bucle = true;
+            int orden = 0;
+            for (int i = 0; i < jugadores; i++)
+            {
+                
+                Console.Write("ingrese el puntaje del jugador " + (i + 1) +" =  ");
+                int puntos = int.Parse(Console.ReadLine());
+                losJugadores[i] = puntos;
+            }
+            for (int i = 0; i < jugadores - 1; i++)
+
+            {
+                for (int t = 0; t < jugadores - 1; t++)
+                {
+                    if (losJugadores[t] < losJugadores[t + 1])
+                    {
+                        orden = losJugadores[t + 1];
+                        losJugadores[t + 1] = losJugadores[t];
+                        losJugadores[t] = orden;
+                    }
+                }
+            }
+            Console.Clear();
+            for (int i = 0; i < jugadores; i++)
+            {
+                Console.WriteLine(losJugadores[i]);
+                Console.WriteLine("..........................");
+            }
+            Console.ReadKey();
+        }
+    }
+}
