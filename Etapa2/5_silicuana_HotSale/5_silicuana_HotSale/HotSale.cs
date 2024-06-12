0636096a040f2719ab04558a129c25cc2d7fca72
anaucilisA <167359079+anaucilisA@users.noreply.github.com>
2024-06-12 16:46:50 -0300

    Add files via upload

---
+++ Etapa2/5_silicuana_HotSale/5_silicuana_HotSale/HotSale.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_silicuana_HotSale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int productos;
+            Console.Write(" ingrese la cantidad de productos vendidos = ");
+            productos = int.Parse(Console.ReadLine());
+            int[] losProductos = new int[productos];
+            int orden = 0;
+            for (int i = 0; i < productos; i++)
+            { 
+                Console.WriteLine("ingrese el precio del producto " + (i + 1));
+                int puntos = int.Parse(Console.ReadLine());
+                losProductos[i] = puntos;
+            }
+            for (int i = 0; i < productos - 1; i++)
+
+            {
+                for (int t = 0; t < productos - 1; t++)
+                {
+                    if (losProductos[t] < losProductos[t + 1])
+                    {
+                        orden = losProductos[t + 1];
+                        losProductos[t + 1] = losProductos[t];
+                        losProductos[t] = orden;
+                    }
+                }
+            }
+            Console.WriteLine("el precio mas alto es = " + losProductos[0]);
+            Console.WriteLine("el precio mas bajo es = " + losProductos[productos - 1]);
+            for (int i = 0; i < productos; i++)
+            {
+                Console.WriteLine("...........................");
+                Console.WriteLine(losProductos[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa2/6_silicuana_ZEGARPG/6_silicuana_ZEGARPG/ZEGARPG.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_silicuana_ZEGARPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int objetos;
+            int valorM;
+            Console.Write(" ingrese la cantidad de objetos = ");
+            objetos = int.Parse(Console.ReadLine());
+            int[] losObjetos = new int[objetos];
+            Console.Write(" ingrese el valor minimo de los objetos = ");
+            valorM = int.Parse(Console.ReadLine());
+            Console.WriteLine(".....................");
+            for (int i = 0; i < objetos; i++)
+            {
+                Console.WriteLine("ingresa el valor minimo del obejeto " + (i + 1));
+                int valor = int.Parse(Console.ReadLine());
+                losObjetos[i] = valor;
+            }
+            Console.WriteLine(".....................");
+            Console.WriteLine("objetos que superan al valor minimo:");
+            for (int i = 0; i < objetos; i++)
+            {
+                if (losObjetos[i] > valorM)
+                {
+                    Console.WriteLine("objeto " + (i + 1));
+                }
+            }
+            Console.WriteLine("objetos que no  superan al valor minimo:");
+            for (int i = 0; i < objetos; i++)
+            { 
+                if (losObjetos[i] < valorM)
+                {
+                    Console.WriteLine("objeto " + (i + 1));
+                }
+        }
+        //final
+
+        Console.ReadKey();
+        }
+    }
+}
