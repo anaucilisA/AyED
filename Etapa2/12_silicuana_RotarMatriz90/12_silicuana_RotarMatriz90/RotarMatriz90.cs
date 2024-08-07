2d974b7339b16dc1e5121a27b6264bb4e0838361
anaucilisA <167359079+anaucilisA@users.noreply.github.com>
2024-08-07 16:59:14 -0300

    Add files via upload

---
+++ Etapa2/12_silicuana_RotarMatriz90/12_silicuana_RotarMatriz90/RotarMatriz90.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_silicuana_RotarMatriz90
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ramdom = new Random();
+
+            Console.Write("ingrese la cantidad de filas: ");
+            int f = int.Parse(Console.ReadLine());
+            Console.Write("ingrese la cantidad de columnas: ");
+            int c = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[f, c];
+            int[,] matriz2 = new int[c, f];
+            Console.WriteLine();
+            Console.WriteLine("matriz normal");
+            Console.WriteLine();
+            for (int i = 0; i < f; i++)
+            {
+                for (int t = 0; t < c; t++)
+                {
+                    matriz[i, t] = ramdom.Next(1, 100);
+                    matriz2[t, i] = matriz[i, t];
+                    Console.Write(matriz[i, t] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("matriz rotada 90 grados");
+            Console.WriteLine();
+            for (int i = 0; i < c; i++)
+            {
+                for (int t = 0; t < f; t++)
+                {
+
+                    Console.Write(matriz[t, i] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa2/13_silicuana_ObtenerDiagonalPrincipal/13_silicuana_ObtenerDiagonalPrincipal/ObtenerDiagonalPrincipal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_silicuana_ObtenerDiagonalPrincipal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+
+
+            int[,] matriz = new int[n, n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = random.Next(1, 10);
+                }
+            }
+
+
+            Console.WriteLine("Matriz generada:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+
+            int[] diagonalPrincipal = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                diagonalPrincipal[i] = matriz[i, i];
+            }
+
+
+            Console.WriteLine("\nDiagonal Principal:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(diagonalPrincipal[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+  
