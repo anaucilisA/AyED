1cc895494d42677fdef1fbd5f91cafe789ae118d
anaucilisA <167359079+anaucilisA@users.noreply.github.com>
2024-10-30 14:44:37 -0300

    Add files via upload

---
+++ Etapa3/0_silicuana_MiPrimerFuncion/0_silicuana_MiPrimerFuncion/MiPrimerFuncion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_silicuana_MiPrimerFuncion
+{
+    class Program
+    {
+        static string nombre(string po)
+        {
+            string saludos = ("hola" + po);
+            return saludos;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(nombre(" andy"));
+            Console.ReadKey();
+        }
+    }
+}
